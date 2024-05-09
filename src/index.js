3df819bec0e4883f7f9078244119a251b7f583a1
kiran-hutech <36165693+kiranpoojary@users.noreply.github.com>
2024-05-10 00:01:26 +0530

    done graphql

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// node-graphql/src/index.js
+
+const { ApolloServer } = require("apollo-server");
+const { typeDefs } = require("./schema");
+const { resolvers } = require("./resolver");
+
+const port = process.env.PORT || 9090;
+
+const server = new ApolloServer({ resolvers, typeDefs });
+
+//GUIDE IS HERE--------------
+// https://dev.to/nditah/how-to-build-a-graphql-api-with-node-prisma-and-postgres-ajg
+
+server.listen({ port }, () =>
+  console.log(`Server runs at: http://localhost:${port}`)
+);
