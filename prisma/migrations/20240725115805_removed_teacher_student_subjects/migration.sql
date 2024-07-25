ce4242d9bdd1e8289ac07d877da4acd34fcf6722
kiran-hutech <108514946+kiranhutech@users.noreply.github.com>
2024-07-25 17:28:49 +0530

    removed old student apis

---
+++ prisma/migrations/20240725115805_removed_teacher_student_subjects/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Student` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Subject` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Teacher` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "Student";
+
+-- DropTable
+DROP TABLE "Subject";
+
+-- DropTable
+DROP TABLE "Teacher";
